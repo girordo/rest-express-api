dd9ef735390ab7ab60d34819ea793277ea6594bb
girordo <tsgiroldo@gmail.com>
2022-05-29 14:45:01 -0300

    :sparkles: | Implementing user service

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import { createUser } from "../service/user.service";
 import logger from "../utils/logger";
 
-export function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(req: Request, res: Response) {
   try {
+    const user = await createUser(req.body);
   } catch (e) {
     logger.error(e);
-    res.status(409).send(e.message);
+    res.status(409);
   }
 }
