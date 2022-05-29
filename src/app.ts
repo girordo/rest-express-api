ee26270f06489300e0cfb7d76a62d3232c38d10e
girordo <tsgiroldo@gmail.com>
2022-05-29 16:14:10 -0300

    :art: | Improving code quality

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import config from "config";
-import connect from "../utils/connect";
-import logger from "../utils/logger";
-import routes from "./routes";
+import express from 'express'
+import config from 'config'
+import connect from './utils/connect'
+import logger from './utils/logger'
+import routes from './routes'
 
-const port = config.get<number>("port");
+const port = config.get<number>('port')
 
-const app = express();
+const app = express()
 
 app.listen(port, async () => {
-  logger.info(`Server started at http://localhost:${port}`);
-  await connect();
+  logger.info(`Server started at http://localhost:${port}`)
+  await connect()
 
-  routes(app);
-});
+  routes(app)
+})
